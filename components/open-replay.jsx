cb002e72c5332b83694c0db5954f224cc22b1c41
aymaneallaoui <aymanepraxe@gmail.com>
2024-10-26 15:47:31 +0100

    userid

--- components/open-replay.jsx
+++ components/open-replay.jsx
@@ -1,16 +1,24 @@
 'use client'
 import { useEffect } from "react"
 import Tracker from '@openreplay/tracker'
+import { v4 as uuidV4 } from 'uuid'
+
 
 const tracker = new Tracker({
     projectKey: "H6naHVvsrfibVNQ2jnIY",
     ingestPoint: "https://openreplay.kafka-api.site/ingest",
 })
 
+function defaultGetUserId() {
+    return uuidV4()
+}
+
 const Openreplay = () => {
     useEffect(() => {
         if (typeof window !== 'undefined') {
             tracker.start()
+            userId = defaultGetUserId()
+            tracker.setUserID(userId)
         }
     }, [])
 
