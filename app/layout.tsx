177694a7ea0f6a77b49538d7a68d89c355341bc4
aymane aallaoui <111997551+aymaneallaoui@users.noreply.github.com>
2024-10-27 22:20:21 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,6 +33,13 @@ export default function RootLayout({
           <Providers>{children}</Providers>
         </SessionPRO>
         <OpenReplayNoSSR />
+        <Script type="text/javascript">
+          {`(function(w,d,n,u,o,t,m){w['SrecObject']=o;w[o]=w[o]||function(){
+    (w[o].q=w[o].q||[]).push(arguments)},w[o].l=1*new Date();t=d.createElement(n),
+    m=d.getElementsByTagName(n)[0];t.async=1;t.src=u;m.parentNode.insertBefore(t,m)
+    })(window,document,'script','http://uxlens.akera.me/collect/initialize.js','srec');
+    srec('init', '94ead810-94a8-11ef-b237-9d211fa7dc12');`}
+      </Script>
       </body>
     </html>
   );
